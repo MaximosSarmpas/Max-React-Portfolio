a1e1e526687ed80805d748c5e267bad927e78561
Maximos Sarmpas <117605647+MaximosSarmpas@users.noreply.github.com>
2023-05-10 21:57:56 -0400

    Update App.js
    changes made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+// Importing necessary modules and components
 import React, { useState } from "react";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Page from "./components/Page";
 import Footer from "./components/Footer";
 
+// The main App function component
 function App() {
+  
+  // Define the initial state for the pages using useState hook
   const [pages] = useState([
     {
       name: "about me",
